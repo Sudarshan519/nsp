b605eadf8a71ab2fa9044dc18eeda9336e2138ff
Aashish Adhikari <asis.adh@gmail.com>
2021-02-02 11:21:20 +0545

    [chore]: All test passing. Coverage is also scored

--- test/stub/stub.dart
+++ test/stub/stub.dart
@@ -5,5 +5,10 @@ import 'package:flutter/foundation.dart';
 String stub({
   @required String path,
   @required String name,
-}) =>
-    File('test/stub/$path/$name.json').readAsStringSync();
+}) {
+  var dir = Directory.current.path;
+  if (dir.endsWith('/test')) {
+    dir = dir.replaceAll('/test', '');
+  }
+  return File('$dir/test/stub/$path/$name.json').readAsStringSync();
+}
