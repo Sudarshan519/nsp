84e5f73154342f83683a4eaee86c7eb71de6dc6e
Aayush Nikkon SUbedi <aayush.subedi@newitventure.com>
2021-06-02 10:38:50 +0545

    appbar fix in topup page

--- lib/ui/pages/home/widgets/utility_payment/detail_pages/topup/topup_page.dart
+++ lib/ui/pages/home/widgets/utility_payment/detail_pages/topup/topup_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:wallet_app/ui/pages/add_balance/widget/balance_widgets.dart';
-import 'package:wallet_app/ui/pages/add_balance/widget/payment_options.dart';
 import 'package:wallet_app/ui/widgets/colors.dart';
-import 'package:wallet_app/ui/widgets/shodow_box.dart';
 import 'package:another_flushbar/flushbar_helper.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wallet_app/features/load_balance/domain/entities/payment_method.dart';
@@ -46,7 +44,12 @@ class TopUpPage extends StatelessWidget {
       builder: (context, state) {
         return state.map(
           loading: (_) => loadingPage(),
-          loaded: (success) => body(success.data),
+          loaded: (success) => Column(
+            children: [
+              BalanceWidget(balance: success.data.balance ?? 0.0),
+              body(success.data),
+            ],
+          ),
           failure: (error) {
             Future.delayed(Duration.zero, () {
               FlushbarHelper.createError(
@@ -65,107 +68,108 @@ class TopUpPage extends StatelessWidget {
   }
 
   Widget body(LoadFund? loadFund) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          BalanceWidget(balance: loadFund?.balance ?? 0.0),
-          const SizedBox(height: 20),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 15.0),
-            child: Text(
-              'Mobile (10 digits)',
-              style: TextStyle(fontWeight: FontWeight.w600),
-            ),
-          ),
-          Container(
-            height: 36,
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    maxLength: 10,
-                    controller: _phNoController,
-                    keyboardType: TextInputType.number,
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
-                    ],
-                    decoration: InputDecoration(
-                        hintText: "Mobile Number",
-                        hintStyle: TextStyle(fontSize: 13),
-                        counterText: "",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide: const BorderSide(
-                            color: Colors.grey,
-                            width: 0.1,
-                            style: BorderStyle.none,
-                          ),
-                        ),
-                        suffixIcon: IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.contacts_outlined))),
-                  ),
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-                CircleAvatar(
-                  backgroundColor: Palette.primary,
-                  radius: 18,
-                  child: const Icon(
-                    Icons.phone_android,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              shape: BeveledRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 15.0),
+              child: Text(
+                'Mobile (10 digits)',
+                style: TextStyle(fontWeight: FontWeight.w600),
               ),
             ),
-            onPressed: () {},
-            child: Text(
-              'I have a coupon',
-              style: TextStyle(color: Palette.primary),
-            ),
-          ),
-          MaterialButton(
-            minWidth: double.infinity,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0)),
-            onPressed: () {},
-            color: Palette.primary,
-            textColor: Colors.white,
-            child: const Text('Proceed'),
-          ),
-          const Spacer(),
-          OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                  textStyle: const TextStyle(color: Colors.black)),
-              onPressed: () {},
+            Container(
+              height: 36,
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    'My Payments (1)',
-                    style: TextStyle(color: Colors.black),
+                children: [
+                  Expanded(
+                    child: TextField(
+                      maxLength: 10,
+                      controller: _phNoController,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                      ],
+                      decoration: InputDecoration(
+                          hintText: "Mobile Number",
+                          hintStyle: TextStyle(fontSize: 13),
+                          counterText: "",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                              width: 0.1,
+                              style: BorderStyle.none,
+                            ),
+                          ),
+                          suffixIcon: IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.contacts_outlined))),
+                    ),
                   ),
-                  Icon(
-                    Icons.keyboard_arrow_up_outlined,
-                    color: Colors.black,
-                  )
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Palette.primary,
+                    radius: 18,
+                    child: const Icon(
+                      Icons.phone_android,
+                      color: Colors.white,
+                    ),
+                  ),
                 ],
-              ))
-        ],
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                shape: BeveledRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              onPressed: () {},
+              child: Text(
+                'I have a coupon',
+                style: TextStyle(color: Palette.primary),
+              ),
+            ),
+            MaterialButton(
+              minWidth: double.infinity,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)),
+              onPressed: () {},
+              color: Palette.primary,
+              textColor: Colors.white,
+              child: const Text('Proceed'),
+            ),
+            const Spacer(),
+            OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                    textStyle: const TextStyle(color: Colors.black)),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      'My Payments (1)',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    Icon(
+                      Icons.keyboard_arrow_up_outlined,
+                      color: Colors.black,
+                    )
+                  ],
+                ))
+          ],
+        ),
       ),
     );
   }
