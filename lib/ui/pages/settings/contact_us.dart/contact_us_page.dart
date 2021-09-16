9ebd1ddc5b320e2cc04c967da71566e288194b92
Aayush Nikkon SUbedi <aayush.subedi@newitventure.com>
2021-09-16 18:20:41 +0545

    ui changes in contact us page

--- lib/ui/pages/settings/contact_us.dart/contact_us_page.dart
+++ lib/ui/pages/settings/contact_us.dart/contact_us_page.dart
@@ -1,7 +1,6 @@
 import 'package:another_flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:wallet_app/features/contact_us/presentation/contact_us/contact_us_bloc.dart';
 import 'package:wallet_app/features/home/presentation/home_page_data/home_page_data_bloc.dart';
@@ -27,99 +26,135 @@ class ContactUsPage extends StatelessWidget {
     final name = '${detail?.firstName ?? ''} ${detail?.lastName ?? ''}';
     final email = detail?.email ?? '';
 
-    Widget formWidget() {
-      return BlocProvider(
-          create: (context) => getIt<ContactUsBloc>()
-            ..add(ContactUsEvent.setInital(name: name, email: email)),
-          child: BlocConsumer<ContactUsBloc, ContactUsState>(
-            listener: (context, state) {
-              state.failureOrSuccessOption.fold(
-                () => {},
-                (either) => either.fold(
-                  (failure) {
-                    FlushbarHelper.createError(
-                        message: failure.map(
-                      serverError: (error) => error.message,
-                      invalidUser: (_) => AppConstants.someThingWentWrong,
-                      noInternetConnection: (_) => AppConstants.noNetwork,
-                    )).show(context);
-                  },
-                  (success) {
-                    showDialog(
-                      barrierDismissible: false,
-                      context: context,
-                      builder: (_) => PopUpSuccessOverLay(
-                        title: "Success",
-                        message: 'Your message has been succesfully delivered!',
-                        onPressed: () {
-                          context.router.navigate(const TabBarRoute());
-                        },
-                      ),
-                    );
-                  },
+    Widget bodyWidget() {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Contact Us",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          leading: const BackButton(color: Colors.white),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const _FormBody(),
+                const SizedBox(height: 8),
+                _infoWidget(
+                  onTap: () => launcher.launch('mailto:info@bnpj.jp'),
+                  icon: Icons.email,
+                  title: 'Mail Us',
+                  description: 'info@bnpj.jp',
                 ),
-              );
-            },
-            builder: (context, state) {
-              return state.isLoading ? loadingPage() : const _FormBody();
-            },
-          ));
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Contact Us",
-          style: TextStyle(
-            color: Colors.white,
+                _infoWidget(
+                  onTap: () => launcher.launch('tel:03-6222-8365'),
+                  icon: Icons.call,
+                  title: 'Call Us',
+                  description: '03-6222-8365',
+                ),
+                _infoWidget(
+                  icon: Icons.location_on,
+                  title: 'Visit Us',
+                  description:
+                      'NAX Building 9F, Shinkawa 1-3-2, Chuo-ku , Tokyo 104-0033',
+                ),
+                const SizedBox(height: 8),
+                const SizedBox(
+                  height: 300,
+                  child: GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                          target: LatLng(
+                            35.6773408,
+                            139.7774822,
+                          ),
+                          zoom: 17)),
+                )
+              ],
+            ),
           ),
         ),
-        leading: const BackButton(color: Colors.white),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            formWidget(),
-            Card(
-              child: ListTile(
-                onTap: () => launcher.launch('mailto:info@bnpj.jp'),
-                leading: Icon(Icons.email, color: Palette.primary),
-                title: const Text('Mail Us'),
-                subtitle: const Text('info@bnpj.jp'),
+      );
+    }
+
+    return BlocProvider(
+        create: (context) => getIt<ContactUsBloc>()
+          ..add(ContactUsEvent.setInital(name: name, email: email)),
+        child: BlocConsumer<ContactUsBloc, ContactUsState>(
+          listener: (context, state) {
+            state.failureOrSuccessOption.fold(
+              () => {},
+              (either) => either.fold(
+                (failure) {
+                  FlushbarHelper.createError(
+                      message: failure.map(
+                    serverError: (error) => error.message,
+                    invalidUser: (_) => AppConstants.someThingWentWrong,
+                    noInternetConnection: (_) => AppConstants.noNetwork,
+                  )).show(context);
+                },
+                (success) {
+                  showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (_) => PopUpSuccessOverLay(
+                      title: "Success",
+                      message: 'Your message has been succesfully delivered!',
+                      onPressed: () {
+                        context.router.navigate(const TabBarRoute());
+                      },
+                    ),
+                  );
+                },
               ),
-            ),
-            Card(
-              child: ListTile(
-                onTap: () => launcher.launch('tel:03-6222-8365'),
-                leading: Icon(Icons.call, color: Palette.primary),
-                title: const Text('Call Us'),
-                subtitle: const Text('03-6222-8365'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.location_on, color: Palette.primary),
-                title: const Text('Visit Us'),
-                subtitle: const Text(
-                    'NAX Building 9F, Shinkawa 1-3-2, Chuo-ku , Tokyo 104-0033'),
-              ),
-            ),
-            const SizedBox(height: 8),
-            const SizedBox(
-              height: 300,
-              child: GoogleMap(
-                  initialCameraPosition: CameraPosition(
-                      target: LatLng(
-                        35.6773408,
-                        139.7774822,
-                      ),
-                      zoom: 17)),
-            )
-          ],
-        ),
-      ),
-    );
+            );
+          },
+          builder: (context, state) {
+            return state.isLoading
+                ? Container(color: Colors.white, child: loadingPage())
+                : bodyWidget();
+          },
+        ));
   }
+}
+
+Widget _infoWidget(
+    {required IconData icon,
+    required String title,
+    required String description,
+    void Function()? onTap}) {
+  return Card(
+      child: Padding(
+    padding: const EdgeInsets.all(12.0),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Icon(icon, color: Palette.primary),
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: const TextStyle(fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  description,
+                  textScaleFactor: 0.87,
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  ));
 }
 
 class _FormBody extends StatelessWidget {
@@ -130,7 +165,6 @@ class _FormBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ShadowBoxWidget(
-      margin: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextWidetWithLabelAndChild(
